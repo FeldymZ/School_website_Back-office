d6d5668942003bd2239592d7bd990845b5c3cf9d
FeldymZ <feldy.moundzegou@gmail.com>
2026-09-23 07:49:46 +0100

    mise a jour back-office

--- src/layout/AdminLayout.tsx
+++ src/layout/AdminLayout.tsx
@@ -4,15 +4,14 @@ import Topbar from "../components/Topbar";
 
 const AdminLayout = () => {
   return (
-    <div className="h-screen bg-gray-100 overflow-hidden">
-      {/* Sidebar FIXE */}
-      <div className="fixed left-0 top-0 h-screen w-64 z-30">
-        <Sidebar />
-      </div>
+    <div className="h-screen bg-gray-100 overflow-hidden flex">
+      {/* Sidebar : gère elle-même sa largeur (w-0 ↔ w-72/w-64 selon sidebarOpen) */}
+      <Sidebar />
 
-      {/* Zone principale (décalée à droite) */}
-      <div className="ml-64 h-screen flex flex-col">
-        {/* Topbar FIXE */}
+      {/* Zone principale : flex-1 prend automatiquement tout l'espace restant,
+          se redimensionne en temps réel avec l'animation de la sidebar */}
+      <div className="flex-1 min-w-0 h-screen flex flex-col">
+        {/* Topbar FIXE (dans le flux, sticky au scroll) */}
         <div className="sticky top-0 z-20">
           <Topbar />
         </div>
